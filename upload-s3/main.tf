a2cedda3cd21b1f6df45a1db099cfbc198acbf17
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-15 13:43:05 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -72,7 +72,7 @@ resource "null_resource" "push_filebrowser_image" {
   provisioner "local-exec" {
     command = <<EOT
       # Clean up any previous build directory
-      sudo chown ${whoami} . /home
+      sudo chown $(whoami) . /home
       rm -rf /home/filebrowser-build || true
       mkdir -p /home/filebrowser-build
       cd /home/filebrowser-build
