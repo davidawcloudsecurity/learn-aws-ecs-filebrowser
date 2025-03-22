1b184a9b805596ca63fffb0e7c9997fd83d8d949
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-22 17:36:07 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -221,11 +221,11 @@ USER root
 
 RUN apk add --no-cache s3fs-fuse fuse ca-certificates bash
 RUN mkdir -p /srv/s3bucket
-ENV S3_BUCKET=\${aws_s3_bucket.filebrowser_storage.bucket}
+ENV S3_BUCKET=${aws_s3_bucket.filebrowser_storage.bucket}
 
 RUN echo '#!/bin/bash' > /entrypoint.sh && \
     echo 'set -e' >> /entrypoint.sh && \
-    echo 'echo "Mounting S3 bucket: \$S3_BUCKET"' >> /entrypoint.sh && \
+    echo 'echo "Mounting S3 bucket: $S3_BUCKET"' >> /entrypoint.sh && \
     echo 's3fs "\$S3_BUCKET" /srv/s3bucket -o iam_role=auto -o allow_other -o umask=0022 -o dbglevel=info' >> /entrypoint.sh && \
     echo 'echo "S3 bucket mounted at /srv/s3bucket using IAM role"' >> /entrypoint.sh && \
     echo 'export FB_ROOT=/srv/s3bucket' >> /entrypoint.sh && \
