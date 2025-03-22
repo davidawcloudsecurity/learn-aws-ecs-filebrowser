280936ad39f428a8faf773801fc28d783338b062
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-22 19:56:34 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -17,6 +17,8 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "192.168.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"  # Explicitly set a supported AZ
+
   tags = {
     Name = "filebrowser-public-subnet"
   }
