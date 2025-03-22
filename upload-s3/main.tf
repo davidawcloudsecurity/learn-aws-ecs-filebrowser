94203144a4be194239433b0f944cbc0fef36046e
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-22 19:57:58 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "192.168.1.0/24"
+  cidr_block              = "192.168.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"  # Explicitly set a supported AZ
 
