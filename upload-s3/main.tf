2d05d2849ce07b3b98f98e37ff75cafab08862c2
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-22 19:26:18 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -119,13 +119,13 @@ resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
-
+/*
 # Create instance profile
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "ec2_instance_profile"
   role = aws_iam_role.ec2_role.name
 }
-
+*/
 # ECS Cluster
 resource "aws_ecs_cluster" "filebrowser_cluster" {
   name = "filebrowser-cluster"
