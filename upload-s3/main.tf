d260bf43a58a845f33a056f57846270e7cf8ac2c
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2025-03-22 19:20:03 +0800

    Update main.tf

--- upload-s3/main.tf
+++ upload-s3/main.tf
@@ -359,6 +359,22 @@ EOF
   }
 }
 
+resource "aws_lb_target_group" "ecs_target_group" {
+  name     = "ecs-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
+
 resource "aws_launch_template" "ecs" {
   name = "ecs-launch-template"
 
